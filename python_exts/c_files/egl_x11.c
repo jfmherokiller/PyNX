d4ffcad68a7c46e65764115095cdaa9121e9981d
noah the goodra <peterpan0413@live.com>
2018-10-31 10:36:24 -0500

    add opengl portion of renpy

--- python_exts/c_files/egl_x11.c
+++ python_exts/c_files/egl_x11.c
@@ -6,8 +6,6 @@
 
 #include "eglsupport.h"
 
-Window window;
-
 EGLDisplay display;
 EGLSurface surface;
 EGLConfig config;
@@ -42,72 +40,11 @@ char *egl_error(char *where) {
  * an error message on failure.
  */
 char *egl_init(SDL_Window *sdl_window, int interval) {
-    SDL_SysWMinfo wminfo;
-    EGLint major, minor;
-    EGLint num_config;
-
-    const EGLint attrs[] = {
-         EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
-         EGL_ALPHA_SIZE, 8,
-         EGL_NONE
-     };
-
-    const EGLint context_attrs[] = {
-        EGL_CONTEXT_CLIENT_VERSION, 2,
-        EGL_NONE
-    };
-
-    SDL_VERSION(&wminfo.version);
-    SDL_GetWindowWMInfo(sdl_window, &wminfo);
-
-    if (! initialized) {
-
-    	display = eglGetDisplay(wminfo.info.x11.display);
-		egl_check("getting display");
-
-		if (display == EGL_NO_DISPLAY) {
-			return "EGL display not found.";
-		}
-
-		eglInitialize(display, &major, &minor);
-		egl_check("initializing EGL");
-
-		eglBindAPI(EGL_OPENGL_ES_API);
-		egl_check("binding OpenGL ES");
-
-		eglChooseConfig(display, attrs, &config, 1, &num_config);
-		egl_check("choosing EGL config");
-
-		context = eglCreateContext(display, config, EGL_NO_CONTEXT, context_attrs);
-		egl_check("creating EGL context");
-
-    	surface = eglCreateWindowSurface(display, config, (EGLNativeWindowType) wminfo.info.x11.window, NULL);
-    	egl_check("creating EGL surface");
-
-    } else if (window != wminfo.info.x11.window) {
-
-    	eglDestroySurface(display, surface);
-    	egl_check("destroying existing EGL surface")
-
-    	surface = eglCreateWindowSurface(display, config, (EGLNativeWindowType) wminfo.info.x11.window, NULL);
-    	egl_check("creating EGL surface");
-
-    }
-
-	eglMakeCurrent(display, surface, surface, context);
-	egl_check("making EGL context current");
-
-    eglSwapInterval(display, interval);
-    egl_check("setting swap interval")
-
-	initialized = 1;
-	window = wminfo.info.x11.window;
-
     return NULL;
 }
 
 void egl_swap() {
-	eglSwapBuffers(display, surface);
+
 }
 
 void egl_quit() {
